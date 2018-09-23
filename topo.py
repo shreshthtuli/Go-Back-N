773e353ddbf2880eb827596eeb46fd060ab6e5ed
shreshthtuli <34661516+shreshthtuli@users.noreply.github.com>
2018-09-23 17:14:33 +0530

    topo now working

--- topo.py
+++ topo.py
@@ -4,12 +4,12 @@ Mininet Topologies with 2 nodes
 Author : Shreshth Tuli
 
 Usage:
-    - sudo mn --custom topo.py --controller=remote,ip=127.0.0.1
+    - sudo mn --custom topo.py --topo linear --controller=remote,ip=127.0.0.1
 
 To specify parameters use: --link tc,bw=10,delay=3,loss=2,max_queue_size=3
 
 Example : for ring topology with bandwidth limited to 2:
-	- sudo mn --custom topo.py --controller=remote,ip=127.0.0.1 --link tc,bw=10
+	- sudo mn --custom topo.py --topo linear --controller=remote,ip=127.0.0.1 --link tc,bw=10
 
 """
 
@@ -20,19 +20,34 @@ from mininet.link import TCLink
 from mininet.util import dumpNodeConnections
 from mininet.log import setLogLevel
 
-class SimpleTopo( Topo ):
-    "Simple Linear Topology Mininet"
+class LinearTopo( Topo ):
+    "Linear Topology Mininet"
 
     def __init__( self ):
 
         # Initialize topology
         Topo.__init__( self )
 
-        h1 = self.addHost('h1', ip='0.0.0.1')
-        h2 = self.addHost('h2', ip='0.0.0.2')
-        s1 = self.addSwitch('s1')
+        hosts = []
+        switches = []
 
-        self.addLink(h1, s1)
-        self.addLink(h2, s1)   
+        for x in range(0, 10):
 
-topos = SimpleTopo()
+            # Add hosts and switches
+            hosts.append(self.addHost( 'h%s' % (x+1) ))
+            switches.append(self.addSwitch( 's%s' % (x+1) ))
+
+            # Add links in linear topology
+
+            # Connecting hosts to switches
+            self.addLink(hosts[x], switches[x])
+            # Connecting switches in linear order
+            if(x > 0):
+                self.addLink(switches[x-1], switches[x]) 
+
+topos = { 
+ 'linear': ( lambda: LinearTopo() ),
+ 'ring': ( lambda: RingTopo() ) ,
+ 'mesh': ( lambda: MeshTopo() ) ,
+ 'star': ( lambda: StarTopo()) 
+  }
