edd146712ff27e36a3bee9f7273c0adcd579f82f
Samarth Aggarwal <samarthaggarwal@Samarths-MacBook-Pro.local>
2018-09-24 14:24:57 +0530

    correct testing file

--- testing.sh
+++ testing.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 
-if [$1 == 0]
+if [ $1 -eq 0 ]
 then
 	echo time python3 host.py 0 $2 photo.png a.txt
-elif [$1 == 1]
-then
+else
 	echo time python3 host.py 1 $2 photo.png b.txt
 fi
