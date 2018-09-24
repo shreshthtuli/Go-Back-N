25b6550653177bc038c4d16d09bceb36e2c57de9
Samarth Aggarwal <samarthaggarwal@Samarths-MacBook-Pro.local>
2018-09-24 16:13:27 +0530

    removed timeout error

--- sender.py
+++ sender.py
@@ -1,30 +1,19 @@
-'''
-
-Host file for sending and receiving data using Go Back N protocol
-Authors:
-    - Shreshth Tuli
-    - Samarth Aggarwal
-
-Usage-
-    python3 host.py 0_or_1 window_size file_to_send file_to_write
-    
-'''
+# sender.py - The sender in the reliable data transfer protocol
 import packet
 import socket
 import sys
 import _thread
 import time
 import udt
-import random
 
 from timer import Timer
 
-PACKET_SIZE = 1
+PACKET_SIZE = 512
+RECEIVER_ADDR = ('localhost', 8080)
+SENDER_ADDR = ('localhost', 0)
 SLEEP_INTERVAL = 0.05
-TIMEOUT_INTERVAL = 0.1
-WINDOW_SIZE = 7
-timeouts = 0
-loss = 0
+TIMEOUT_INTERVAL = 0.5
+WINDOW_SIZE = 4
 
 # Shared resources across threads
 base = 0
@@ -53,7 +42,6 @@ def send(sock, filename):
     packets = []
     seq_num = 0
     while True:
-        PACKET_SIZE = random.randint(32, 221)
         data = file.read(PACKET_SIZE)
         if not data:
             break
@@ -68,14 +56,12 @@ def send(sock, filename):
 
     # Start the receiver thread
     _thread.start_new_thread(receive, (sock,))
-    timeouts = 0
 
     while base < num_packets:
         mutex.acquire()
         # Send all the packets in the window
         while next_to_send < base + window_size:
             print('Sending packet', next_to_send)
-            print('Sending to ', RECEIVER_ADDR)
             udt.send(packets[next_to_send], sock, RECEIVER_ADDR)
             next_to_send += 1
 
@@ -93,13 +79,11 @@ def send(sock, filename):
 
         if send_timer.timeout():
             # Looks like we timed out
-            timeouts = timeouts + 1
-            print('Timeout', timeouts)
+            print('Timeout')
             send_timer.stop();
             next_to_send = base
         else:
             print('Shifting window')
-            print('Number of Timeouts', timeouts)
             window_size = set_window_size(num_packets)
         mutex.release()
 
@@ -121,62 +105,44 @@ def receive(sock):
 
     expected_num = 0
     while True:
-        pkt, addr = udt.recv(sock);
+        pkt, _ = udt.recv(sock);
         seq_num, ack, data = packet.extract(pkt)
-
-        print('Got packet with address = ',addr)
 
         # If we get an ACK for the first in-flight packet
         if(ack >= 1):
             print('Got ACK', seq_num)
             if (seq_num >= base):
                 mutex.acquire()
-                base = seq_num + 1
+                base = ack + 1
                 print('Base updated', base)
                 send_timer.stop()
                 mutex.release()
 
         else:
-            print('Got DATA', seq_num)
+            print('Gor DATA', seq_num)
             if seq_num == expected_num:
-                mutex.acquire()
                 print('Got expected packet')
-                print('Count : ', expected_num)
                 print('Sending ACK', expected_num)
                 pkt = packet.make(expected_num, 1)
                 udt.send(pkt, sock, addr)
                 expected_num += 1
                 file.write(data)
-                mutex.release()
             else:
-                mutex.acquire()
-                print('Did not get expected packet', expected_num)
                 print('Sending ACK', expected_num - 1)
                 pkt = packet.make(expected_num - 1, 1)
                 udt.send(pkt, sock, addr)
-                mutex.release()
 
 
 # Main function
 if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print('Expected filename as command line argument')
+        exit()
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-    
-    RECEIVER_ADDR = ('localhost', 8080)
-    SENDER_ADDR = ('localhost', 1200)
-    if(sys.argv[1] == '1'):
-        RECEIVER_ADDR = ('localhost', 1200)
-        SENDER_ADDR = ('localhost', 8080)
-
-    WINDOW_SIZE = int(sys.argv[2])
-    
-    print(SENDER_ADDR);
     sock.bind(SENDER_ADDR)
+    filename = sys.argv[1]
+    writefilename = sys.argv[2]
 
-    filename = sys.argv[3]
-    writefilename = sys.argv[4]
-    # loss = int(sys.argv[5])
-    
     send(sock, filename)
     sock.close()
