9feb393f43696a55f3d397e20a08388eef25cc1a
shreshthtuli <34661516+shreshthtuli@users.noreply.github.com>
2018-09-24 21:53:43 +0530

    updated testing script

--- testing.sh
+++ testing.sh
@@ -3,7 +3,7 @@
 
 if [ $1 -eq 0 ]
 then
-	time python3 host.py 0 $2 Programming\ Assignment\ 2.pdf a.txt $3
+	time python3 host.py 0 $2 photo.png a.png $3
 else
-	time python3 host.py 1 $2 Programming\ Assignment\ 2.pdf b.txt $3
+	time python3 host.py 1 $2 photo.png b.png $3
 fi
