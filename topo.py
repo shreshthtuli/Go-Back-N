626704d4b073c08ca742d62a22c2f540f396b268
shreshthtuli <34661516+shreshthtuli@users.noreply.github.com>
2018-09-24 13:28:44 +0530

    corrected topo.py

--- topo.py
+++ topo.py
@@ -28,13 +28,16 @@ class LinearTopo( Topo ):
         # Initialize topology
         Topo.__init__( self )
 
-        h1 = self.addHost('h1')
-        h2 = self.addHost('h2')
-        s1 = self.addSwitch('s1')
-        s2 = self.addSwitch('s2')
-        self.addLink(h1, s1)
-        self.addLink(h2, s2)
-        self.addLink(s2, s2)
+        # Add hosts and switches
+        leftHost = self.addHost( 'h1' )
+        rightHost = self.addHost( 'h2' )
+        leftSwitch = self.addSwitch( 's3' )
+        rightSwitch = self.addSwitch( 's4' )
+
+        # Add links
+        self.addLink( leftHost, leftSwitch )
+        self.addLink( leftSwitch, rightSwitch )
+        self.addLink( rightSwitch, rightHost )
 
 topos = { 
  'linear': ( lambda: LinearTopo() ),
