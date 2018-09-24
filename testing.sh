3e1d873be9555e5d0b73908f6dc6baff1cde6c53
Samarth Aggarwal <samarth_aggarwal@yahoo.co.in>
2018-09-24 04:45:42 -0700

    testing using photo.png

--- testing.sh
+++ testing.sh
@@ -3,7 +3,7 @@
 
 if [ $1 -eq 0 ]
 then
-	time python3 host.py 0 $2 README.md a.txt $3
+	time python3 host.py 0 $2 photo.png a.txt $3
 else
-	time python3 host.py 1 $2 README.md b.txt $3
+	time python3 host.py 1 $2 photo.png b.txt $3
 fi
