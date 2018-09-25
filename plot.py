b32fe3f789e9e9d98f2256384120ce3c570bc000
Samarth Aggarwal <samarthaggarwal2510@gmail.com>
2018-09-25 23:11:29 +0530

    completed color combination and mutliplot

--- plot.py
+++ plot.py
@@ -1,11 +1,5 @@
 import matplotlib.pyplot as plt 
 
-# x=[1,2,3,4]
-# y=[1,4,9,16]
-
-# plt.plot(x,y)
-# plt.show()
-
 th=[12.506,12.253,12.193,16.921,16.995,17.035,21.703,21.568,21.015]
 latency=[10.156,10.397,10.551,7.4269,7.4728,7.4891,5.7986,5.8795,6.0491]
 
@@ -15,16 +9,25 @@ delays=[0,3,5]
 fig, ax1 = plt.subplots()
 ax2 = ax1.twinx()
 
-ax1.plot(delays, th[0:3], 'r-', alpha=0.3)
-ax1.plot(delays, th[3:6], 'r-', alpha=0.7)
-ax1.plot(delays, th[6:9], 'r-', alpha=1)
-ax2.plot(delays, latency[0:3], 'b-', alpha=0.3)
-ax2.plot(delays, latency[3:6], 'b-', alpha=0.7)
-ax2.plot(delays, latency[6:9], 'b-', alpha=1)
+ax1.plot(delays, th[0:3], 'r-', alpha=0.3, label="window size 5")
+# plt.legend()
+ax1.plot(delays, th[3:6], 'r-', alpha=0.7, label="window size 7")
+# plt.legend()
+ax1.plot(delays, th[6:9], 'r-', alpha=1, label="window size 9")
+# ax1.legend()
+ax2.plot(delays, latency[0:3], 'b-', alpha=0.3, label="window size 5")
+# ax2.legend()
+ax2.plot(delays, latency[3:6], 'b-', alpha=0.7, label="window size 7")
+# ax2.legend()
+ax2.plot(delays, latency[6:9], 'b-', alpha=1, label="window size 9")
+# ax2.legend()
+
 
 ax1.set_xlabel('Delay (in ms)')
-ax1.set_ylabel('Throughput (in KBps)', color='g')
+ax1.set_ylabel('Throughput (in KBps)', color='r')
 ax2.set_ylabel('Latency (in ms)', color='b')
+ax1.legend(loc='upper right', framealpha=0.8)
+ax2.legend(loc='lower right', framealpha=0.8)
 plt.title('Drop Rate = 1%')
 
 # line plots
@@ -41,7 +44,7 @@ plt.title('Drop Rate = 1%')
 # plt.ylabel('Throughput (in KBps)')
 # plt.title('Drop Rate = 1%')
 # showing legend 
-plt.legend()
+# plt.legend()
   
 # function to show the plot 
 plt.show()
