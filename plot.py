88591a2439d55a9b2478b2a380591305ca116e84
Samarth Aggarwal <samarthaggarwal2510@gmail.com>
2018-09-25 22:25:05 +0530

    added two y-axis for throughput and latency

--- plot.py
+++ plot.py
@@ -7,25 +7,35 @@ import matplotlib.pyplot as plt
 # plt.show()
 
 th=[12.506,12.253,12.193,16.921,16.995,17.035,21.703,21.568,21.015]
+latency=[10.156,10.397,10.551,7.4269,7.4728,7.4891,5.7986,5.8795,6.0491]
 
 delays=[0,3,5]
-plt.ylim(0,25)
+# plt.ylim(0,25)
 
-plt.plot(delays, th[0:3], label="window size = 5", color="red")
-plt.plot(delays, th[3:6], label="window size = 7", color="green")
-plt.plot(delays, th[6:9], label="window size = 9", color="blue")
+fig, ax1 = plt.subplots()
+ax2 = ax1.twinx()
 
+ax1.plot(delays, th[0:3], 'g-')
+ax2.plot(delays, latency[0:3], 'b-')
 
+ax1.set_xlabel('Delay (in ms)')
+ax1.set_ylabel('Throughput (in KBps)', color='g')
+ax2.set_ylabel('Latency (in ms)', color='b')
+plt.title('Drop Rate = 1%')
+
+# line plots
+# plt.plot(delays, th[0:3], label="window size = 5", color="red")
+# plt.plot(delays, th[3:6], label="window size = 7", color="green")
+# plt.plot(delays, th[6:9], label="window size = 9", color="blue")
+
+# scatter plots
 # plt.scatter(delays, th[0:3], label="window size = 5", color="red")
 # plt.scatter(delays, th[3:6], label="window size = 7", color="green")
 # plt.scatter(delays, th[6:9], label="window size = 9", color="blue")
 
-# x-axis label 
-plt.xlabel('Delay (in ms)') 
-# frequency label 
-plt.ylabel('Throughput (in KBps)')
-# plot title 
-plt.title('Drop Rate = 1%')
+# plt.xlabel('Delay (in ms)') 
+# plt.ylabel('Throughput (in KBps)')
+# plt.title('Drop Rate = 1%')
 # showing legend 
 plt.legend()
   
